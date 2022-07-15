f856f7d4a0a57fb3bfd3d327c37498bb8a52b59d
Krishnakant Sharma <48293816+kksharmaa@users.noreply.github.com>
2022-07-15 18:03:38 +0530

    'interstate'

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 // import {SafeAreaProvider} from 'react-native-safe-area-context';
 // import * as eva from '@eva-design/eva';
 // import {ApplicationProvider} from '@ui-kitten/components';
-import AgentHome from './src/screens/AgentHome';
+import Route from './src/screens/Route';
 
 export default function App() {
   return (
@@ -17,7 +17,7 @@ export default function App() {
     //   </ApplicationProvider>
     // </Provider>
     <>
-      <AgentHome />
+      <Route />
     </>
   );
 }
